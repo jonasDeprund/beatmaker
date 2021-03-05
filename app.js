90179d4cb5bbfd3e7823492b295e5a04142357ab
Jonas Deprund <jonasdeprund@gmail.com>
2021-03-05 16:42:14 +0100

    active states done

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll('.pad')
+    this.playBtn = document.querySelector('.play')
     this.kickAudio = document.querySelectorAll('.kick-sound')
     this.snareAudio = document.querySelectorAll('.snare-sound')
     this.hihatAudio = document.querySelectorAll('.hihat-sound')
     this.index = 0
+    this.bpm = 150
   }
+
+  activePad() {
+    this.classList.toggle('active')
+  }
+
   repeat() {
     let step = this.index % 8
     const activeBars = document.querySelectorAll(`.b${step}`)
-    console.log(activeBars)
+    console.log(step)
     this.index++
   }
 
   start() {
+    const interval = (60 / this.bpm) * 1000
     setInterval(() => {
       this.repeat()
-    }, 1000)
+    }, interval)
   }
 }
 
 const drumKit = new DrumKit()
 
-drumKit.start()
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener('click', drumKit.activePad)
+})
+
+drumKit.playBtn.addEventListener('click', function () {
+  drumKit.start()
+})
