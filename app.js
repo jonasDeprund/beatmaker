8e6b7675f9e767338a2e925e2dd6124ef1116b19
Jonas Deprund <jonasdeprund@gmail.com>
2021-03-13 16:50:15 +0100

    loop play/stop done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ class DrumKit {
     this.hihatAudio = document.querySelector('.hihat-sound')
     this.index = 0
     this.bpm = 150
+    this.isPlaying = null
   }
 
   activePad() {
@@ -39,9 +40,23 @@ class DrumKit {
 
   start() {
     const interval = (60 / this.bpm) * 1000
-    setInterval(() => {
-      this.repeat()
-    }, interval)
+    if (!this.isPlaying) {
+      this.isPlaying = setInterval(() => {
+        this.repeat()
+      }, interval)
+    } else {
+      clearInterval(this.isPlaying)
+      this.isPlaying = null
+    }
+  }
+  updateBtn() {
+    if (!this.isPlaying) {
+      this.playBtn.innerText = 'Stop'
+      this.playBtn.classList.add('active')
+    } else {
+      this.playBtn.innerText = 'Play'
+      this.playBtn.classList.remove('active')
+    }
   }
 }
 
@@ -55,5 +70,6 @@ drumKit.pads.forEach((pad) => {
 })
 
 drumKit.playBtn.addEventListener('click', function () {
+  drumKit.updateBtn()
   drumKit.start()
 })
